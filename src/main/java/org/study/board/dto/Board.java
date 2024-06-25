b172fd17359b75a3acc3b0711fa0ab18ab5e3b04
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-06-25 14:22:54 +0900

    feat : 답변형 게시판 1차

--- src/main/java/org/study/board/dto/Board.java
+++ src/main/java/org/study/board/dto/Board.java
@@ -1,10 +1,12 @@
 package org.study.board.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
+//@Builder
 @Data
 public class Board {
 
@@ -17,6 +19,12 @@ public class Board {
     private boolean deleteYn;
     private Integer userIdx;
     private Integer boardType; // 0: 공지사항 게시판, 1: QnA 게시판
+
+    //계층형 게시판
+    private Integer gno;    // 그룹 번호, 원글(부모글)과 답변글은 동일한 gno를 가진다.
+    private Integer sorts;  // 정렬 순서
+    private Integer depth;  // 답변글이면 원글(부모글)의 depth + 1을, 원글이면 0을 가진다.
+    private Integer parentBno;
 
     /*페이징 기능*/
     private Integer pageNo;	    // 현재 페이지 번호
