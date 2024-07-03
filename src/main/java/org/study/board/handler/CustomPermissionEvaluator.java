e8fc1197efff9d48825392eeed52aa242ffa166f
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-07-03 17:15:21 +0900

    chore : 사이드바 카테고리 오류수정

--- src/main/java/org/study/board/handler/CustomPermissionEvaluator.java
+++ src/main/java/org/study/board/handler/CustomPermissionEvaluator.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.study.board.dto.User;
 import org.study.board.dto.UserCtgAuth;
 import org.study.board.repository.AdminMapper;
 import org.study.board.service.CustomUserDetailsService;
